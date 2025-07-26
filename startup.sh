09f63da0dfdd2e162d7c8f69ae4de170570bba6a
Chakolito <ckhyle27@gmail.com>
2025-07-26 11:59:37 +0800

    change directory in startup code

--- startup.sh
+++ startup.sh
@@ -13,7 +13,6 @@ python3 ./AI_Files/FastAPI_Endpoint/main.py &
 # Wait briefly to ensure FastAPI starts
 sleep 3
 
-# Start ASP.NET Core app (must bind to expected port)
+# Start ASP.NET Core app
 echo "Starting ASP.NET Core..."
-dotnet VisemoServices.dll --urls=http://0.0.0.0:80
-#test
+dotnet /app/VisemoServices.dll --urls=http://0.0.0.0:80
